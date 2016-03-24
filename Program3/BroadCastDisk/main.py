972ce108d2ec1e0332cb30c315ba56f8ff4493ef
dntfg4 <dntfg4@mst.edu>
2016-03-24 00:25:49 -0500

    LIX
    LIX

--- Program3/BroadCastDisk/main.py
+++ Program3/BroadCastDisk/main.py
@@ -6,14 +6,14 @@ from PIX import PIX
 
 if __name__ == "__main__":
     bdisk = BroadCastDisk()
-    lix_data = ""
+    lix_client_data = ""
+    lix_broadcast = ""
+    lix_disks = []
+    lix_disks_frequency = []
+    lix_cache_size = 1
     pix_client_data = ""
     pix_broadcast = ""
     pix_cache_size = 1
-    perform_lix = False
-    perform_pix = False
-    user_input = []
-    i = 0
 
     if len(sys.argv) == 2:
         fp = open(sys.argv[1])
@@ -22,32 +22,52 @@ if __name__ == "__main__":
             if len(line) > 0:
                 line_split = line.split(':')
                 data = line_split[0]
-                if line_split[0] == "LIX":
-                    perform_lix = True
-                    lix_data = line_split[1]
+                if line_split[0] == "LIXClient":
+                    lix_client_data = line_split[1]
+                elif line_split[0] == "LIXBroadcast":
+                    lix_broadcast = line_split[1]
+                elif line_split[0] == "LIXDisk":
+                    lix_disks.append(line_split[1])
+                    lix_disks_frequency.append(int(line_split[2]))
+                elif line_split[0] == "LIXCacheSize":
+                    lix_cache_size = int(line_split[1])
+                    if lix_cache_size < 1:
+                        lix_cache_size = 1
                 elif line_split[0] == "PIXClient":
-                    perform_pix = True
                     pix_client_data = line_split[1]
                 elif line_split[0] == "PIXBroadcast":
-                    perform_pix = True
                     pix_broadcast = line_split[1]
                 elif line_split[0] == "PIXCacheSize":
                     pix_cache_size = int(line_split[1])
                     if pix_cache_size < 1:
                         pix_cache_size = 1
-                else:
-                    percentage = eval(compile(line_split[1], '<string>', 'eval', __future__.division.compiler_flag))
-                    user_input.append((line_split[0], percentage))
-                    bdisk.add_data(line_split[0], percentage)
+                elif line_split[0] == "BroadCastDisk":
+                    percentage = eval(compile(line_split[2], '<string>', 'eval', __future__.division.compiler_flag))
+                    bdisk.add_data(line_split[1], percentage)
     else:
         print "Usage: python main.py <input file>"
 
-    broadcast = bdisk.generate_schedule()
+    if bdisk.get_number_of_data() > 0:
+        broadcast = bdisk.generate_schedule()
 
-    if perform_pix:
+    if (len(pix_broadcast) > 0) and (len(pix_client_data) > 0):
         pix = PIX(pix_broadcast, pix_client_data, pix_cache_size)
         pix.perform()
+    elif (len(pix_broadcast) > 0) or (len(pix_client_data) > 0):
+        print "\nPIX File Entries must have the following:"
+        print "     1. A line which has something like \'PIXClient:a,b,c,b,a,a\'"
+        print "     2. A line which has something like \'PIXBroadcast:a,b,a,c,a,d\'"
+        print "\n The PIX Entry similar to \'PIXCacheSize:2\' is optional. \'PIXCacheSize:1\' is default"
 
-    if perform_lix:
-        lix = LIX(user_input, broadcast, lix_data)
+    if (len(lix_broadcast) > 0) and (len(lix_client_data) > 0) and (len(lix_disks) > 0):
+        lix = LIX(lix_broadcast, lix_client_data, lix_disks, lix_disks_frequency, lix_cache_size)
         lix.perform()
+    elif (len(lix_broadcast) > 0) or (len(lix_client_data) > 0) or (len(lix_disks) > 0):
+        print "\nLIX File Entries must have the following:"
+        print "     1. A line which has something like \'LIXClient:a,b,c,b,a,a\'"
+        print "     2. A line which has something like \'LIXBroadcast:a,b,a,c,a,d\'"
+        print "     3. At least one line which has something similar to \'LIXDisk:b,c,d:1\'"
+        print "        where \'b,c,d\' are the elements in the disk and 1 is the frequency."
+        print "        The disks entries, as a whole, must contain all of the proper elements in the broadcast;"
+        print "        otherwise, program may crash."
+        print "\n The LIX Entry similar to \'LIXCacheSize:2\' is optional. \'LIXCacheSize:1\' is default"
